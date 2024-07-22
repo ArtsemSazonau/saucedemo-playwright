20ff80ade867099e6be45e061f50200196a25e9b
Artsem Sazonau <artsem.sazonau@gmail.com>
2024-07-23 00:09:56 +0300

    no errors

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -18,12 +18,12 @@ test('get started link', async ({ page }) => {
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
 
-test('error test', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+// test('error test', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation_error' })).toBeVisible();
-});
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'Installation_error' })).toBeVisible();
+// });
